2fa08165d207144b482126b6aa5d70b32dd93bdb
Aniket-1906 <aniket@progton.com>
2020-09-24 18:40:39 +0530

    Created Blog App and added features

---
+++ app/controllers/authors/posts_controller.rb
@@ -0,0 +1,75 @@
+module Authors
+
+class PostsController < AuthorsController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = current_author.posts
+  end
+
+  # GET /posts/1
+  def show
+  end
+
+
+
+  def like
+    @post = Post.find(params[:id])
+    #raise @post.like_count.inspect
+    like = @post.like_count.nil? ? 0.to_i : @post.like_count
+    #raise like.inspect
+    like_new = like + 1.to_i
+@post.update_attributes(:like_count => like_new)
+    redirect_to posts_path
+  end
+
+  # GET /posts/new
+  def new
+    @post = current_author.posts.build
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+    @post = current_author.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = current_author.posts.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+end
+
+
+end
